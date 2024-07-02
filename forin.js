e4948c4af654e84b6d489618b97ec28a4200304a
Aswini Sreeja <aswinisreeja.j@gmail.com>
2024-07-02 14:56:37 +0530

    git commit-m "commit"

---
+++ forin.js
@@ -0,0 +1,9 @@
+var details={
+    Name:'Aswini',
+    College:'Arunachala',
+}
+for (let key in details)
+{
+    console.log(key+details);
+
+}
--- reverse.js
+++ reverse.js
@@ -1,6 +1,6 @@
 let i=10
-while(i>0)
-{
+
+do{
  console.log(i)
      i--
-}
+}while(i>0)
