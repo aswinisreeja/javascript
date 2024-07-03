9f679425c356633a58d367c7359afbb198c086c6
aswinisreeja <aswinisreeja.j@gmail.com>
2024-07-03 12:27:15 +0530

    Add files via upload

---
+++ day/ifcondit.js
@@ -0,0 +1,10 @@
+var a=77
+if(a>10)
+    console.log(a/11) 
+var a=77
+if(a<10)
+    console.log(a/11)
+else if(a<=77)
+    console.log(a/7)
+else 
+    console.log("it cant")
---
+++ day/numberrange.js
@@ -0,0 +1,11 @@
+var mark=70;
+if(mark>=90)
+    console.log("grade A")
+else if (mark>=80)
+    console.log("grade B")
+else if(mark>=70)
+    console.log("grade C")
+else if(mark>=60)
+    console.log("grade D")
+else
+    console.log("fail")
