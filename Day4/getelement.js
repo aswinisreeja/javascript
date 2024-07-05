d967693dc3617a91d11165ba2f3a7f14dada2b3d
aswinisreeja <aswinisreeja.j@gmail.com>
2024-07-05 09:16:12 +0530

    Add files via upload

---
+++ Day4/getelement.js
@@ -0,0 +1,7 @@
+var changein=document.getElementById("one")
+    console.log(changein.textContent)
+    
+function change()
+{
+    changein.textContent="Bye"
+}
---
+++ Day4/javascript.js
@@ -0,0 +1,5 @@
+
+function handClick()
+{
+alert("the function is clicked")
+}
